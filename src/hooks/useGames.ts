6af6bc2e97947ffcbc576a5fdacf12602863f113
Tian <143532687+pxisbest@users.noreply.github.com>
2025-02-28 20:45:55 +0800

    display credit score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,19 +9,18 @@ export interface Platform {
     
 }
 
-
 export interface Game{
     id:number;
     name:string;
     background_image:string;
     parent_platforms:{platform:Platform}[]
+    metacritic:number;
 }
 
 interface FetchGamesResponse{
     count:number;
     results:Game[]
 }
-
 
 const useGames = () => {
     const [games, setGames]=useState<Game[]>([]);
