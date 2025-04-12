65381e0281ff0a2a6146e7c5f90ed85e55fd959a
Tian <143532687+pxisbest@users.noreply.github.com>
2025-04-13 02:36:50 +0800

    minor ui change

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ const GameCard = ({game}:Props) => {
   return (
     <Card.Root>
         <Image src={game.background_image}/>
-        <CardBody>
+        <CardBody _dark={{ backgroundColor: "gray.800" }}>
             <Heading fontSize="2xl">{game.name}</Heading>
             <HStack justifyContent="space-between">
             <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
